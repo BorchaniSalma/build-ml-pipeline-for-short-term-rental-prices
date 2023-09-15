2e3ed0912c3c94ac49e8e66975d5c964e49367e3
BorchaniSalma <sborcheni@ieee.org>
2023-09-15 17:18:35 -0600

    pipeline complete

--- components/test_regression_model/run.py
+++ components/test_regression_model/run.py
@@ -1,7 +1,13 @@
-#!/usr/bin/env python
 """
-This step takes the best model, tagged with the "prod" tag, and tests it against the test dataset
+This step takes the best model, tagged with the "prod" tag, and
+tests it against the test dataset
+
+Date: 09/15/2023
+Author: Salma Borchani
+
 """
+
+# Importing the packages
 import argparse
 import logging
 import wandb
@@ -9,24 +15,32 @@ import mlflow
 import pandas as pd
 from sklearn.metrics import mean_absolute_error
 
-from wandb_utils.log_artifact import log_artifact
-
-
 logging.basicConfig(level=logging.INFO, format="%(asctime)-15s %(message)s")
 logger = logging.getLogger()
 
 
 def go(args):
+    """
+    This function fetch two artifacts
+      1: Inference artifact ( our saved ML model )
+      2: Test data artifact ( test datasets )
+
+    then we load the ML model and score it against our test datasets
+    the we store the metrics such as r2 and mae to the run summary of wandb
+
+    """
 
     run = wandb.init(job_type="test_model")
     run.config.update(args)
 
     logger.info("Downloading artifacts")
-    # Download input artifact. This will also log that this script is using this
+
+    # Downloading the inference artifact. This will also log that this script is using this
     # particular version of the artifact
     model_local_path = run.use_artifact(args.mlflow_model).download()
 
-    # Download test dataset
+    logger.info("fetching the test artifact path")
+    # Downloading test dataset
     test_dataset_path = run.use_artifact(args.test_dataset).file()
 
     # Read test dataset
@@ -34,10 +48,12 @@ def go(args):
     y_test = X_test.pop("price")
 
     logger.info("Loading model and performing inference on test set")
+    # Loading the ML model
     sk_pipe = mlflow.sklearn.load_model(model_local_path)
     y_pred = sk_pipe.predict(X_test)
 
     logger.info("Scoring")
+    # Scoring the model
     r_squared = sk_pipe.score(X_test, y_test)
 
     mae = mean_absolute_error(y_test, y_pred)
@@ -52,18 +68,19 @@ def go(args):
 
 if __name__ == "__main__":
 
-    parser = argparse.ArgumentParser(description="Test the provided model against the test dataset")
+    parser = argparse.ArgumentParser(
+        description="Test the provided model against the test dataset")
 
     parser.add_argument(
         "--mlflow_model",
-        type=str, 
+        type=str,
         help="Input MLFlow model",
         required=True
     )
 
     parser.add_argument(
         "--test_dataset",
-        type=str, 
+        type=str,
         help="Test dataset",
         required=True
     )
--- main.py
+++ main.py
@@ -104,27 +104,44 @@ def go(config: DictConfig):
             )
         if "train_random_forest" in active_steps:
 
-            # NOTE: we need to serialize the random forest configuration into JSON
+            # we need to serialize the random forest configuration into JSON
             rf_config = os.path.abspath("rf_config.json")
             with open(rf_config, "w+") as fp:
-                json.dump(dict(config["modeling"]["random_forest"].items()), fp)  # DO NOT TOUCH
+                json.dump(
+                    dict(
+                        config["modeling"]["random_forest"].items()),
+                    fp)
 
-            # NOTE: use the rf_config we just created as the rf_config parameter for the train_random_forest
-            # step
+            # running the train_random_forest component
+            _ = mlflow.run(
+                os.path.join(hydra.utils.get_original_cwd(), "src", "train_random_forest"),
+                "main",
+                # version = "main"
+                parameters={
+                    "trainval_artifact": 
+                     config["parameters"]["train_random_forest"]["trainval_artifact"],
+                    "val_size": config["modeling"]["val_size"],
+                    "random_seed": config["modeling"]["random_seed"],
+                    "stratify_by": config["modeling"]["stratify_by"],
+                    "rf_config": rf_config,
+                    "max_tfidf_features": config["modeling"]["max_tfidf_features"],
+                    "output_artifact": 
+                     config["parameters"]["train_random_forest"]["output_artifact"],
+                }
+            )
 
-            ##################
-            # Implement here #
-            ##################
-
-            pass
 
         if "test_regression_model" in active_steps:
-
-            ##################
-            # Implement here #
-            ##################
-
-            pass
+            # running the test_regression_model
+            _ = mlflow.run(
+                os.path.join(hydra.utils.get_original_cwd(), "components", "test_regression_model"),
+                "main",
+                # version = "main"
+                parameters={
+                    "mlflow_model": config["parameters"]["test_regression_model"]["mlflow_model"],
+                    "test_dataset": config["parameters"]["test_regression_model"]["test_dataset"]
+                }
+            )
 
 
 if __name__ == "__main__":
